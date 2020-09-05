07d65427def3dfd9713f978c619e46dd71f4a1c8
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2020-09-05 17:29:43 +1000

    Updating solution structure

---
+++ AdafruitSoilMoistureReader/App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AdafruitSoilMoistureReader.App
+{
+    internal class Program
+    {
+        public static string RunPythonCommand(string command, string pythonExecPath, string args)
+        {
+            var start = new ProcessStartInfo
+            {
+                FileName = pythonExecPath,
+                Arguments = $"\"{command}\" \"{args}\"",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using var process = Process.Start(start);
+            using var reader = process?.StandardOutput;
+
+            string stderr = process?.StandardError.ReadToEnd();
+            string result = reader?.ReadToEnd();
+            return result;
+        }
+
+        private static void Main(string[] args)
+        {
+            var res = RunPythonCommand("read_temp_and_soil_moisture.py", args[0], null);
+            Console.WriteLine(res);
+        }
+    }
+}
