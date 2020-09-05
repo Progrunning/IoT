31fc8867bbba70735cc2116371041df929da5415
Mikolaj Kieres <mikolaj.kieres@gmail.com>
2020-09-05 20:07:31 +1000

    Adding some logging around sending message to IoT hub

--- AdafruitSoilMoistureReader/Core/Services/IotHubService.cs
+++ AdafruitSoilMoistureReader/Core/Services/IotHubService.cs
@@ -4,21 +4,29 @@ using System.Threading.Tasks;
 using AdafruitSoilMoistureReader.Core.Interfaces;
 using AdafruitSoilMoistureReader.Core.Models;
 using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Logging;
 
 namespace AdafruitSoilMoistureReader.Core.Services
 {
     public class IotHubService : IIotHubService
     {
         private readonly DeviceClient _deviceClient;
+        private readonly ILogger<IotHubService> _logger;
 
-        public IotHubService(IAdafruitSoilMoistureReaderConfiguration adafruitSoilMoistureReaderConfiguration)
+        public IotHubService(IAdafruitSoilMoistureReaderConfiguration adafruitSoilMoistureReaderConfiguration, ILogger<IotHubService> logger)
         {
+            _logger = logger;
             _deviceClient = DeviceClient.CreateFromConnectionString(adafruitSoilMoistureReaderConfiguration.ConnectionString);
         }
 
         public async Task SendAdafruitSoilMoistureReading(AdafruitSoilMoistureSensorReading reading)
         {
-            await _deviceClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(reading))));
+            var readingJson = JsonSerializer.Serialize(reading);
+            _logger.LogDebug($"[{nameof(SendAdafruitSoilMoistureReading)}] Sending {readingJson} message to IoT hub...");
+
+            await _deviceClient.SendEventAsync(new Message(Encoding.UTF8.GetBytes(readingJson)));
+
+            _logger.LogDebug($"[{nameof(SendAdafruitSoilMoistureReading)}] Message sent.");
         }
     }
 }
